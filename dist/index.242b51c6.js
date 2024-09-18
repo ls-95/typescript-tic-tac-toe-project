72eba5ba39d70d367e89749acfa8b63b0ef3799f
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-09-18 13:55:57 +0200

    Added main content and styling

--- dist/index.242b51c6.js
+++ dist/index.242b51c6.js
@@ -1,3 +1,18 @@
-console.log("I am here");
+const board = document.querySelector(".game-container");
+function main() {
+    createBoard();
+}
+function createBoard() {
+    for(let i = 0; i < 9; i++)makeBox(i);
+}
+function makeBox(i) {
+    //creating the first box
+    const box = document.createElement("div");
+    box.className = "box";
+    box.id = `box-${i}`; //creating a different id for each box created in the for loop.
+    box.textContent = "X";
+    board.append(box);
+}
+main();
 
 //# sourceMappingURL=index.242b51c6.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("I am here");
+const board = document.querySelector(".game-container") as HTMLElement;
+
+function main(): void {
+  createBoard();
+}
+
+function createBoard(): void {
+  for (let i = 0; i < 9; i++) makeBox(i);
+}
+
+function makeBox(i: number): void {
+  //creating the first box
+  const box: HTMLDivElement = document.createElement("div");
+  box.className = "box";
+  box.id = `box-${i}`; //creating a different id for each box created in the for loop.
+  box.textContent = "X";
+  board.append(box);
+}
+
+main();
