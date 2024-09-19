bc5fb083e1a869897e9ad4ae0453ff6b3f1e9d55
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-09-19 16:11:40 +0200

    Created alternating symbols when box clicked and also a winner when three in a row

--- dist/index.242b51c6.js
+++ dist/index.242b51c6.js
@@ -1,4 +1,5 @@
 const board = document.querySelector(".game-container");
+let turn = "X";
 function listenBoard() {
     board.addEventListener("click", runGame);
 }
@@ -10,7 +11,32 @@ function runGame(e) {
     const boxId = e.target.id;
     console.log(boxId);
     if (boxId === null) return;
-    const box = document.querySelector(`#${boxId}`);
+    const box = document.querySelector(`#${boxId}`); // This is to obtain the id of the specific box clicked.
+    if (box === null || box.textContent !== "") return;
+    box.textContent = turn;
+    const winner = checkWinner();
+    //The reason for the switch to work perfectly and not change if box clicked twice is because this is called after all of the box checks.
+    //It won't switch the player when clicked on the box again.
+    if (!winner) switchPlayer();
+    else console.log("THERE IS A WINNER");
+}
+function checkWinner() {
+    const boxes = getBoxes();
+    return boxes[0] === boxes[1] && boxes[1] === boxes[2] && boxes[0] !== "" || boxes[3] === boxes[4] && boxes[4] === boxes[5] && boxes[3] !== "" || boxes[6] === boxes[7] && boxes[7] === boxes[8] && boxes[6] !== "" || boxes[0] === boxes[4] && boxes[4] === boxes[8] && boxes[0] !== "" || boxes[2] === boxes[4] && boxes[4] === boxes[6] && boxes[2] !== "" || boxes[1] === boxes[4] && boxes[4] === boxes[7] && boxes[1] !== "" || boxes[0] === boxes[3] && boxes[3] === boxes[6] && boxes[0] !== "" || boxes[2] === boxes[5] && boxes[5] === boxes[8] && boxes[2] !== "";
+}
+function getBoxes() {
+    const boxesContent = [];
+    for(let i = 0; i < 9; i++){
+        const box = document.querySelector(`#box-${i}`);
+        const boxContent = box.textContent;
+        if (boxContent === null) boxesContent.push("");
+        else boxesContent.push(boxContent);
+    }
+    return boxesContent;
+}
+function switchPlayer() {
+    if (turn === "X") turn = "O";
+    else turn = "X";
 }
 function createBoard() {
     for(let i = 0; i < 9; i++)makeBox(i);
@@ -20,7 +46,7 @@ function makeBox(i) {
     const box = document.createElement("div");
     box.className = "box";
     box.id = `box-${i}`; //creating a different id for each box created in the for loop.
-    box.textContent = "X";
+    box.textContent = "";
     board.append(box);
 }
 main();
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 const board = document.querySelector(".game-container") as HTMLElement;
 
+type Turn = "X" | "O" | "";
+
+let turn: Turn = "X";
+
 function listenBoard(): void {
   board.addEventListener("click", runGame);
 }
@@ -13,7 +17,53 @@ function runGame(e: Event): void {
   const boxId: string | null = (<HTMLElement>e.target).id;
   console.log(boxId);
   if (boxId === null) return;
-  const box: HTMLElement | null = document.querySelector(`#${boxId}`);
+  const box: HTMLElement | null = document.querySelector(`#${boxId}`); // This is to obtain the id of the specific box clicked.
+  if (box === null || box.textContent !== "") return;
+  box.textContent = turn;
+  const winner: boolean = checkWinner();
+  //The reason for the switch to work perfectly and not change if box clicked twice is because this is called after all of the box checks.
+  //It won't switch the player when clicked on the box again.
+  if (!winner) {
+    switchPlayer();
+  } else {
+    console.log("THERE IS A WINNER");
+  }
+}
+
+function checkWinner(): boolean {
+  const boxes: Array<string> = getBoxes();
+  return (
+    (boxes[0] === boxes[1] && boxes[1] === boxes[2] && boxes[0] !== "") ||
+    (boxes[3] === boxes[4] && boxes[4] === boxes[5] && boxes[3] !== "") ||
+    (boxes[6] === boxes[7] && boxes[7] === boxes[8] && boxes[6] !== "") ||
+    (boxes[0] === boxes[4] && boxes[4] === boxes[8] && boxes[0] !== "") ||
+    (boxes[2] === boxes[4] && boxes[4] === boxes[6] && boxes[2] !== "") ||
+    (boxes[1] === boxes[4] && boxes[4] === boxes[7] && boxes[1] !== "") ||
+    (boxes[0] === boxes[3] && boxes[3] === boxes[6] && boxes[0] !== "") ||
+    (boxes[2] === boxes[5] && boxes[5] === boxes[8] && boxes[2] !== "")
+  );
+}
+
+function getBoxes(): Array<string> {
+  const boxesContent: Array<string> = [];
+  for (let i = 0; i < 9; i++) {
+    const box = document.querySelector(`#box-${i}`) as HTMLElement;
+    const boxContent: string | null = box.textContent;
+    if (boxContent === null) {
+      boxesContent.push("");
+    } else {
+      boxesContent.push(boxContent);
+    }
+  }
+  return boxesContent;
+}
+
+function switchPlayer(): void {
+  if (turn === "X") {
+    turn = "O";
+  } else {
+    turn = "X";
+  }
 }
 
 function createBoard(): void {
@@ -25,7 +75,7 @@ function makeBox(i: number): void {
   const box: HTMLDivElement = document.createElement("div");
   box.className = "box";
   box.id = `box-${i}`; //creating a different id for each box created in the for loop.
-  box.textContent = "X";
+  box.textContent = "";
   board.append(box);
 }
 
